f8708ee4e449a936133c052308be7cdd2efa2832
oliver-feighan <oliverfeighan1996@gmail.com>
2021-06-04 11:57:08 +0100

    run TDDFT

--- write_files.sh
+++ write_files.sh
@@ -6,9 +6,11 @@ for frame in $(seq 1 50 151); do
 		monomer=trunc_bchla_${i}_frame_${frame}
 		echo $monomer
 
-		sed "s/NAME/${monomer}/g" templates/BCHLA_template.in > Bchla_xTB/Bchla_${monomer}.in
-		sed "s/NAME/${monomer}/g" templates/BCHLA_template.sub > Bchla_xTB/Bchla_${monomer}.sub
+		#sed "s/NAME/..\/monomer_xyzs\/${monomer}/g" templates/Bchla_template.in > Bchla_xTB/Bchla_${monomer}.in
+		#sed "s/NAME/${monomer}/g" templates/Bchla_template.sub > Bchla_xTB/Bchla_${monomer}.sub
 
+		#sed "s/NAME/..\/monomer_xyzs\/${monomer}/g" templates/PBE0_template.in > PBE0/PBE0_${monomer}.in
+		#sed "s/NAME/${monomer}/g" templates/PBE0_template.sub > PBE0/PBE0_${monomer}.sub
 
 		for j in $(seq ${i} 27); do
 			
@@ -19,12 +21,11 @@ for frame in $(seq 1 50 151); do
 			dimer=trunc_bchla_${i}_bchla_${j}_frame_${frame}
 			echo $dimer
 
-			sed "s/NAME/${dimer}/g" templates/PBE0_template.in > PBE0/PBE0_${dimer}.in	
+			#sed "s/NAME/..\/dimer_xyzs\/${dimer}/g" templates/Bchla_template.in > Bchla_xTB/Bchla_${dimer}.in	
+			#sed "s/NAME/${dimer}/g" templates/Bchla_template.sub > Bchla_xTB/Bchla_${dimer}.sub
+			
+			sed "s/NAME/..\/dimer_xyzs\/${dimer}/g" templates/PBE0_template.in > PBE0/PBE0_${dimer}.in	
 			sed "s/NAME/${dimer}/g" templates/PBE0_template.sub > PBE0/PBE0_${dimer}.sub
-
-
-			sed "s/NAME/${dimer}/g" templates/Bchla_template.in > Bchla_xTB/Bchla_${dimer}.in	
-			sed "s/NAME/${dimer}/g" templates/Bchla_template.sub > Bchla_xTB/Bchla_${dimer}.sub
 		done
 	done
 done		
