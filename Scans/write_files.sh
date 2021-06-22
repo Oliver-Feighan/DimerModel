f94c86cd20ffce8c4fc6da9e0fee67cb99416d9e
oliver-feighan <oliverfeighan1996@gmail.com>
2021-06-22 09:02:53 +0100

    run BLYP TDDFT

--- Scans/write_files.sh
+++ Scans/write_files.sh
@@ -5,9 +5,14 @@ for sep in $(cd sep_xyzs; ls *.xyz; cd ../); do
     #sed "s/NAME/${sep/.xyz/}/g" ../templates/Bchla_template.sub > DistanceScans/Bchla_xTB/Bchla_${sep/.xyz/}.sub
     #sed -ie "s/DimerModel\/Bchla_xTB/DimerModel\/Scans\/DistanceScans\/Bchla_xTB/" DistanceScans/Bchla_xTB/Bchla_${sep/.xyz/}.sub
 
-    sed "s/NAME/..\/..\/sep_xyzs\/${sep/.xyz/}/g" ../templates/PBE0_template.in > DistanceScans/PBE0/PBE0_${sep/.xyz/}.in
-    sed "s/NAME/${sep/.xyz/}/g" ../templates/PBE0_template.sub > DistanceScans/PBE0/PBE0_${sep/.xyz/}.sub
-    sed -ie "s/DimerModel\/PBE0/DimerModel\/Scans\/DistanceScans\/PBE0/" DistanceScans/PBE0/PBE0_${sep/.xyz/}.sub
+    sed "s/NAME/..\/..\/sep_xyzs\/${sep/.xyz/}/g" CAMB3LYP_template.in > DistanceScans/CAMB3LYP/CAMB3LYP_${sep/.xyz/}.in
+    sed "s/NAME/${sep/.xyz/}/g" CAMB3LYP_template.sub > DistanceScans/CAMB3LYP/CAMB3LYP_${sep/.xyz/}.sub
+    sed -i "s/DimerModel\/CAMB3LYP/DimerModel\/Scans\/DistanceScans\/CAMB3LYP/" DistanceScans/CAMB3LYP/CAMB3LYP_${sep/.xyz/}.sub
+
+    sed "s/NAME/..\/..\/sep_xyzs\/${sep/.xyz/}/g" BLYP_template.in > DistanceScans/BLYP/BLYP_${sep/.xyz/}.in
+    sed "s/NAME/${sep/.xyz/}/g" BLYP_template.sub > DistanceScans/BLYP/BLYP_${sep/.xyz/}.sub
+    sed -i "s/DimerModel\/BLYP/DimerModel\/Scans\/DistanceScans\/BLYP/" DistanceScans/BLYP/BLYP_${sep/.xyz/}.sub
+
 done
 
 for angle in $(cd angle_xyzs; ls *.xyz; cd ../); do
@@ -17,7 +22,14 @@ for angle in $(cd angle_xyzs; ls *.xyz; cd ../); do
     #sed "s/NAME/${angle/.xyz/}/g" ../templates/Bchla_template.sub > AngleScans/Bchla_xTB/Bchla_${angle/.xyz/}.sub
     #sed -ie "s/DimerModel\/Bchla_xTB/DimerModel\/Scans\/AngleScans\/Bchla_xTB/" AngleScans/Bchla_xTB/Bchla_${angle/.xyz/}.sub
 
-    sed "s/NAME/..\/..\/angle_xyzs\/${angle/.xyz/}/g" ../templates/PBE0_template.in > AngleScans/PBE0/PBE0_${angle/.xyz/}.in
-    sed "s/NAME/${angle/.xyz/}/g" ../templates/PBE0_template.sub > AngleScans/PBE0/PBE0_${angle/.xyz/}.sub
-    sed -ie "s/DimerModel\/PBE0/DimerModel\/Scans\/AngleScans\/PBE0/" AngleScans/PBE0/PBE0_${angle/.xyz/}.sub
+    sed "s/NAME/..\/..\/angle_xyzs\/${angle/.xyz/}/g" CAMB3LYP_template.in > AngleScans/CAMB3LYP/CAMB3LYP_${angle/.xyz/}.in
+    sed "s/NAME/${angle/.xyz/}/g" CAMB3LYP_template.sub > AngleScans/CAMB3LYP/CAMB3LYP_${angle/.xyz/}.sub
+    sed -i "s/DimerModel\/CAMB3LYP/DimerModel\/Scans\/AngleScans\/CAMB3LYP/" AngleScans/CAMB3LYP/CAMB3LYP_${angle/.xyz/}.sub
+
+    sed "s/NAME/..\/..\/angle_xyzs\/${angle/.xyz/}/g" BLYP_template.in > AngleScans/BLYP/BLYP_${angle/.xyz/}.in
+    sed "s/NAME/${angle/.xyz/}/g" BLYP_template.sub > AngleScans/BLYP/BLYP_${angle/.xyz/}.sub
+    sed -i "s/DimerModel\/BLYP/DimerModel\/Scans\/AngleScans\/BLYP/" AngleScans/BLYP/BLYP_${angle/.xyz/}.sub
+
+
+
 done
