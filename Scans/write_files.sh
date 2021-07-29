82c5ce98a72958c284d1ea8c621d287dec4d8701
oliver-feighan <oliverfeighan1996@gmail.com>
2021-07-29 18:25:34 +0100

    run CAMB3LYP

--- Scans/write_files.sh
+++ Scans/write_files.sh
@@ -21,16 +21,16 @@ for angle in $(cd angle_xyzs; ls *.xyz; cd ../); do
     #sed "s/NAME/..\/..\/angle_xyzs\/${angle/.xyz/}/g" Bchla_template.in > AngleScans/Bchla_xTB/Bchla_${angle/.xyz/}.in
     #sed "s/NAME/${angle/.xyz/}/g" Bchla_template.sub > AngleScans/Bchla_xTB/Bchla_${angle/.xyz/}.sub
 
-    #sed "s/NAME/..\/..\/angle_xyzs\/${angle/.xyz/}/g" CAMB3LYP_template.in > AngleScans/CAMB3LYP/CAMB3LYP_${angle/.xyz/}.in
-    #sed "s/NAME/${angle/.xyz/}/g" CAMB3LYP_template.sub > AngleScans/CAMB3LYP/CAMB3LYP_${angle/.xyz/}.sub
+    sed "s/NAME/..\/..\/angle_xyzs\/${angle/.xyz/}/g" CAMB3LYP_template.in > AngleScans/CAMB3LYP/CAMB3LYP_${angle/.xyz/}.in
+    sed "s/NAME/${angle/.xyz/}/g" CAMB3LYP_template.sub > AngleScans/CAMB3LYP/CAMB3LYP_${angle/.xyz/}.sub
     #sed -i "s/DimerModel\/CAMB3LYP/DimerModel\/Scans\/AngleScans\/CAMB3LYP/" AngleScans/CAMB3LYP/CAMB3LYP_${angle/.xyz/}.sub
 
     #sed "s/NAME/..\/..\/angle_xyzs\/${angle/.xyz/}/g" BLYP_template.in > AngleScans/BLYP/BLYP_${angle/.xyz/}.in
     #sed "s/NAME/${angle/.xyz/}/g" BLYP_template.sub > AngleScans/BLYP/BLYP_${angle/.xyz/}.sub
     #sed -i "s/DimerModel\/BLYP/DimerModel\/Scans\/AngleScans\/BLYP/" AngleScans/BLYP/BLYP_${angle/.xyz/}.sub
 
-    sed "s/NAME/..\/..\/angle_xyzs\/${angle/.xyz/}/g" HF_template.in > AngleScans/HF/HF_${angle/.xyz/}.in
-    sed "s/NAME/${angle/.xyz/}/g" HF_template.sub > AngleScans/HF/HF_${angle/.xyz/}.sub
+    #sed "s/NAME/..\/..\/angle_xyzs\/${angle/.xyz/}/g" HF_template.in > AngleScans/HF/HF_${angle/.xyz/}.in
+    #sed "s/NAME/${angle/.xyz/}/g" HF_template.sub > AngleScans/HF/HF_${angle/.xyz/}.sub
     #sed -i -e "s/DimerModel\/HF/DimerModel\/Scans\/AngleScans\/HF/" AngleScans/HF/HF_${angle/.xyz/}.sub
 
 done
