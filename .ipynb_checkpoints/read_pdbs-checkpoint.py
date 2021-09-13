8644f12526d198000b9fd9afb11e44a17f05162a
Oliver-Feighan <oliverfeighan1996@gmail.com>
2021-09-13 18:23:11 +0100

    reorganize exciton runs

--- .ipynb_checkpoints/read_pdbs-checkpoint.py
+++ .ipynb_checkpoints/read_pdbs-checkpoint.py
@@ -13,6 +13,18 @@ def get_BCL_residues(topology):
 
     return result    
     
+def extract_xyz(atom, positions, convert_symbol=False):
+    symbol = atom.element.symbol
+    x = positions[atom.index][0]._value
+    y = positions[atom.index][1]._value
+    z = positions[atom.index][2]._value
+    
+    if not convert_symbol:
+        return symbol, x, y, z
+    else: 
+        return convert_symbol, x, y, z
+    
+    
 def extract_xyz_line(atom, positions, convert_symbol=False):
     symbol = atom.element.symbol
     x = positions[atom.index][0]._value
@@ -23,6 +35,8 @@ def extract_xyz_line(atom, positions, convert_symbol=False):
         return f"{symbol:2s} {x:3.5f} {y:3.5f} {z:3.5f}\n"
     else: 
         return f"{convert_symbol:2s} {x:3.5f} {y:3.5f} {z:3.5f}\n"
+    
+
     
 def write_monomer(residue, positions, file_name, truncate=True):
     positions = positions.in_units_of(unit.angstrom)
@@ -85,6 +99,40 @@ def write_dimer(residue1, residue2, positions, file_name, truncate=True):
     print(header+xyz, file=file)
     file.close()
 
+def get_qcore_xyz(residue, positions):
+    positions = positions.in_units_of(unit.angstrom)
+    
+    index_range = lambda x, y : list(range(x, y + 1))
+    
+    tail_indices = index_range(13, 15) + index_range(46, 65) + index_range(101, 139)
+    C_to_H_index = 13
+    
+    xyz = ""
+    
+    xyz_template = "[{symbol}, {x}, {y}, {z}], "
+    
+    for enum, atom in enumerate(residue.atoms()):
+        if enum not in tail_indices:
+            symbol, x, y, z = extract_xyz(atom, positions)
+            
+            xyz += xyz_template.format(symbol = symbol, x=x, y=y, z=z)
+            
+        if enum == C_to_H_index:
+            symbol, x, y, z = extract_xyz(atom, positions, "H")
+            
+            xyz += xyz_template.format(symbol = symbol, x=x, y=y, z=z)
+    
+    return "[" + xyz[:-2] + "]"
+                                       
+def read_pdb(file_name):
+    pdb = PDBFile(file_name)
+
+    positions = pdb.getPositions(asNumpy = True)
+    topology = pdb.getTopology()
+
+    BCL_residues = get_BCL_residues(topology)
+
+    return BCL_residues, positions
     
 if __name__ == "__main__":
     n_frames = 500
@@ -92,17 +140,10 @@ if __name__ == "__main__":
     for i in range(n_frames):
         frame_number = list(range(1, 50001, 50))[int(1000/n_frames) * i]
     
-        print(frame_number)
-    
         file = f"clean_pdbs/clean_md1_frame_{frame_number}.pdb"
         
-        pdb = PDBFile(file)
-
-        positions = pdb.getPositions(asNumpy = True)
-        topology = pdb.getTopology()
-
-        BCL_residues = get_BCL_residues(topology)
-
+        BCL_resides, positions = read_pdb(file)
+        
         for enum1, residue1 in enumerate(BCL_residues):
             write_monomer(residue1, positions, f"trunc_bchla_{enum1+1}_frame_{frame_number}.xyz")
             
--- Exciton/run_exciton_dimers.py
+++ Exciton/run_exciton_dimers.py
@@ -1,8 +1,49 @@
 import subprocess
 import os
 import json
+import numpy as np
+import sys
+
+from concurrent.futures import ProcessPoolExecutor
+from simtk.openmm.app import *
+from simtk.openmm import *
+
+sys.path.insert(0, '..')
+import read_pdbs
+
 
 QCORE_PATH = os.environ["QCORE_PATH"]
+
+def get_Mg_xyz(res, positions):
+    for enum, atom in enumerate(res.atoms()):
+        if atom.element.symbol == "Mg":
+            xyz = positions[atom.index]
+            
+            return np.array([xyz[0]._value, xyz[1]._value, xyz[2]._value])
+
+def Mg_Mg_distance(BCL_res1, BCL_res2, positions):
+    positions = positions.in_units_of(unit.bohr)
+    
+    Mg1 = get_Mg_xyz(BCL_res1, positions)
+    Mg2 = get_Mg_xyz(BCL_res2, positions)
+    
+    return np.linalg.norm(Mg1 - Mg2)
+
+def read_hex_data(result, results_name, variable):
+    
+    hex_data = result[results_name][variable]["data"]["bytes"]
+    byte_data = bytes.fromhex(" ".join([format(n, "02x") for n in hex_data]))
+                                       
+    dtype = result[results_name][variable]["dtype"]
+    
+    return np.frombuffer(byte_data, dtype=dtype)
+
+def read_indexed_values(result, results_name, variable):
+    
+    data = result[results_name][variable]
+    
+    return data[0]
+    
 
 def run_qcore(qcore_string):
     """
@@ -21,38 +62,85 @@ def run_qcore(qcore_string):
                               executable="/bin/bash",
                               universal_newlines=True)
 
-    json_results = json.loads(json_run.stdout)
+    json_result = json.loads(json_run.stdout)
    
-    print(json_results)
-
+    eigenvalues = read_hex_data(json_result, "res", "eigenvalues")
+    charge_centres = read_hex_data(json_result, "res", "charge_centres")
+    
+    couplings = read_indexed_values(json_result, "res", "couplings_inv_cm")
+    distances = read_indexed_values(json_result, "res", "distances")
+    
+    return {
+        "eigenvalues" : eigenvalues.tolist(),
+        "charge_centres" : charge_centres,
+        "couplings" : couplings,
+        "distances" : distances
+    }
     
     
-    #    arr = np.frombuffer(bytes.fromhex(" ".join([format(n, "02x") for n in data["water"]["transition_dipole"]["data"]["bytes"]]), dtype=data["water"]["transition_dipole"]["dtype"])
-                        
-    #print(json_results)
+def get_data():
 
-    
-
-def get_current_results():
     with open("exciton_data.json") as f:
         return json.load(f)
 
-
-if __name__ == "__main__":
-    #json_file = get_current_results()
+def add_to_data(data, variable, value):
+    data[variable].append(value)
     
-    for frame in range(1, 49951, 100):
-        for i in range(1, 28):
-            for j in range(i+1, 28):
+    return data
 
-                monomerA=f"trunc_bchla_{i}_frame_{frame}.xyz"
-                monomerB=f"trunc_bchla_{j}_frame_{frame}.xyz"
+def dump_data(data):
+    with open("exciton_data.json", 'w') as f:
+        return json.dump(data, f)
+
+    
+def is_and_js():
+    result = []
+    
+    for i in range(1, 28):
+        for j in range(i+1, 28):
+            result.append([i,j])
+    
+    return result
+    
+    
+def write_qcore_str(xyzA, xyzB):
+    qcore_str_template = "res := excitons(structure(xyz = {monomerA}) structure(xyz = {monomerB}) use_xtb = true hamiltonian = states)"
+
+    return qcore_str_template.format(monomerA = xyzA, monomerB = xyzB)
+    
+
+    
+if __name__ == "__main__":
+    data = get_data()
+    
+    ring_assingments = json.load(open("../ring_assignment.json"))
+    assign_ring = lambda i : ring_assingments["rings"][f"{i}"]
                 
-                qcore_string = f"res := excitons(structure(file = '../monomer_xyzs/{monomerA}') structure(file = '../monomer_xyzs/{monomerB}') use_xtb = true hamiltonian = states)"
+    index_tuples = is_and_js()
+    
+    for frame in [1]: #range(1, 1, 100):
+        BCL_residues, positions = read_pdbs.read_pdb(f"../clean_pdbs/clean_md1_frame_{frame}.pdb")
+            
+        distances = list(map(lambda i_j : Mg_Mg_distance(BCL_residues[i_j[0]-1], BCL_residues[i_j[1]-1], positions), index_tuples))
+                    
+        qcore_strings = map(lambda i_j : write_qcore_str(xyzA = read_pdbs.get_qcore_xyz(BCL_residues[i_j[0]-1], positions), xyzB = read_pdbs.get_qcore_xyz(BCL_residues[i_j[1]-1], positions)), index_tuples)
+            
+        with ProcessPoolExecutor(max_workers=20) as pool:
+            qcore_results = list(pool.map(run_qcore, qcore_strings))
+    
+        for enum, qcore_res in enumerate(qcore_results):
+            i, j = index_tuples[enum]
+
+            row = [i, j, assign_ring(i), assign_ring(j), frame, distances[enum], qcore_res["distances"][1], qcore_res["couplings"][1], qcore_res["eigenvalues"], [x - qcore_res["eigenvalues"][0] for x in qcore_res["eigenvalues"][1:]]]
+        
+            for col, value in zip(data.keys(), row):
+                data = add_to_data(data, col, value)
                 
-                run_qcore(qcore_string)
+    dump_data(data)
+    
+    quit(0)
                 
-                exit()
+            
                 
                 
                 
