4e07be60f571ee98c84e00dbde0ac8e2e2406368
Oliver-Feighan <oliverfeighan1996@gmail.com>
2021-09-13 11:37:51 +0100

    run exciton

---
+++ .ipynb_checkpoints/run_exciton_dimers-checkpoint.py
@@ -0,0 +1,9 @@
+import subprocess
+import os
+
+QCORE_PATH = os.environ["QCORE_PATH"]
+
+
+if __name__ == "__main__":
+    
+    
---
+++ run_exciton_dimers.py
@@ -0,0 +1,14 @@
+import subprocess
+import os
+import json
+
+QCORE_PATH = os.environ["QCORE_PATH"]
+
+def get_current_results():
+    
+
+
+if __name__ == "__main__":
+    
+    
+    json_file = get_current_results()
