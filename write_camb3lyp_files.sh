6331f715a2f697e8c2429f72f1ab0027a37a5714
Oliver-Feighan <oliverfeighan1996@gmail.com>
2021-09-14 13:37:15 +0100

    write more camb3lyp

--- write_camb3lyp_files.sh
+++ write_camb3lyp_files.sh
@@ -1,5 +1,16 @@
 for frame in $(seq 1 250 501); do
     for i in $(seq 1 27); do
+
+	monomer=trunc_bchla_${i}_frame_${frame}
+	com_file=CAMB3LYP/CAMB3LYP_${monomer}.com
+	sub_file=CAMB3LYP/CAMB3LYP_${monomer}.sub
+
+	sed "s/NAME/${monomer}/g" templates/CAMB3LYP_template.com > $com_file
+	sed "1d;2d" monomer_xyzs/${monomer}.xyz >> $com_file
+	echo >> $com_file
+	
+	sed "s/NAME/${monomer}/g" templates/CAMB3LYP_template.sub > $sub_file
+
         for j in $(seq ${i} 27); do
             
             if [ ${i} == ${j} ] ; then
