3055b3357d5e1a6bee9afdb2755b845a01196759
oliver-feighan <oliverfeighan1996@gmail.com>
2021-10-12 17:04:32 +0100

    different method for CAMB3LYP transition charges

--- CAMB3LYP/CAMB3LYP_excited_trunc_bchla_18_frame_501.com
+++ CAMB3LYP/CAMB3LYP_excited_trunc_bchla_18_frame_501.com
@@ -1,6 +1,6 @@
 %nproc=24
 %mem=175gb
-#p cam-b3lyp/Def2SVP td=(nstates=5) density=(CIS=1)
+#p cam-b3lyp/Def2SVP td=(nstates=5, root=1) density=current
 
 TDDFT excited states in gaussian
 
--- write_camb3lyp_files.sh
+++ write_camb3lyp_files.sh
@@ -11,18 +11,18 @@ for frame in $(seq 1 250 501); do
 	excited_com_file=CAMB3LYP/CAMB3LYP_excited_${monomer}.com
 	excited_sub_file=CAMB3LYP/CAMB3LYP_excited_${monomer}.sub
 	
-	sed "s/NAME/ground_${monomer}/g" templates/CAMB3LYP_ground_template.com > $ground_com_file
-	sed "1d;2d" monomer_xyzs/${monomer}.xyz >> $ground_com_file
-	echo >> $ground_com_file
+	#sed "s/NAME/ground_${monomer}/g" templates/CAMB3LYP_ground_template.com > $ground_com_file
+	#sed "1d;2d" monomer_xyzs/${monomer}.xyz >> $ground_com_file
+	#echo >> $ground_com_file
 	
-	sed "s/NAME/ground_${monomer}/g" templates/CAMB3LYP_ground_template.sub > $ground_sub_file
+	#sed "s/NAME/ground_${monomer}/g" templates/CAMB3LYP_ground_template.sub > $ground_sub_file
 
 
-	sed "s/NAME/transition_${monomer}/g" templates/CAMB3LYP_template.com > $transition_com_file
-	sed "1d;2d" monomer_xyzs/${monomer}.xyz >> $transition_com_file
-	echo >> $transition_com_file
+	#sed "s/NAME/transition_${monomer}/g" templates/CAMB3LYP_template.com > $transition_com_file
+	#sed "1d;2d" monomer_xyzs/${monomer}.xyz >> $transition_com_file
+	#echo >> $transition_com_file
 	
-	sed "s/NAME/transition_${monomer}/g" templates/CAMB3LYP_transition_template.sub > $transition_sub_file
+	#sed "s/NAME/transition_${monomer}/g" templates/CAMB3LYP_transition_template.sub > $transition_sub_file
 
 
 	sed "s/NAME/excited_${monomer}/g" templates/CAMB3LYP_excited_template.com > $excited_com_file
